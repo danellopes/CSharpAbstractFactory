706af08eaec7e47df9bb708e8ffc3311d18788a6
danellopes <daniel.nunes.meira@outlook.com>
2024-02-03 13:53:57 -0300

    C# Abstract Factories

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿namespace C_AbstractFactory;
+
+public interface IHotDrink
+{
+    void Consume();
+}
+
+internal class Tea : IHotDrink
+{
+    public void Consume()
+    {
+        System.Console.WriteLine("This tea is nice but i'd prefer it with milk.");
+    }
+}
+
+internal class Coffee : IHotDrink
+{
+    public void Consume()
+    {
+        System.Console.WriteLine("This coffee is sensational!");
+    }
+}
+
+public interface IHotDrinkFactory
+{
+    IHotDrink Prepare(int amount);
+}
+
+internal class TeaFactory : IHotDrinkFactory
+{
+    public IHotDrink Prepare(int amount)
+    {
+        System.Console.WriteLine($"Put in a tea bag, boil water, pour {amount} ml, add lemon and enjoy!");
+        return new Tea();
+    }
+}
+
+internal class CoffeeFactory : IHotDrinkFactory
+{
+    public IHotDrink Prepare(int amount)
+    {
+        System.Console.WriteLine($"Grind some beans, boil water, pour {amount} ml, add cream and sugar, enjoy!");
+        return new Coffee();
+    }
+}
+
+public class HotDrinkMachine
+{
+    public enum AvailableDrink
+    {
+        Coffee, Tea
+    }
+
+    private Dictionary<AvailableDrink, IHotDrinkFactory> factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+    public HotDrinkMachine()
+    {
+        foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        {
+            var factory = (IHotDrinkFactory)Activator.CreateInstance(Type.GetType("C_AbstractFactory." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory"));
+            factories.Add(drink, factory);
+        }
+    }
+
+    public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+    {
+        return factories[drink].Prepare(amount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var machine = new HotDrinkMachine();
+        var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 100);
+        drink.Consume();
+    }
+}
